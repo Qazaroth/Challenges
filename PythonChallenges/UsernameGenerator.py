7d3f15b04e3077f2c6d2e6672edd648ab92dfb7d
Qazaroth <65128305+Qazaroth@users.noreply.github.com>
2020-05-11 01:24:45 +0800

    Update UsernameGenerator.py

--- PythonChallenges/UsernameGenerator.py
+++ PythonChallenges/UsernameGenerator.py
@@ -29,4 +29,22 @@ if wordList[0] == "b" and wordList[1] == "'":
 
     print(usernameTemp)
 else:
-    print("no")
+    newWordList = wordList.split('\\n')
+
+    firstName = newWordList[random.randint(0, newWordList.__len__())]
+    secondName = newWordList[random.randint(0, newWordList.__len__())]
+
+    randNo = random.randint(1, 999)
+
+    if randNo < 10:
+        randNo = str("00{r}".format(r=randNo))
+    elif randNo < 90:
+        randNo = str("0{r}".format(r=randNo))
+    else:
+        randNo = str(randNo)
+
+    usernameTemp = "{firstName}{secondName}{randNo}".format(firstName=firstName.capitalize(),
+                                                            secondName=secondName,
+                                                            randNo=randNo)
+
+    print(usernameTemp)
