47edfd12620e4f8d82b39bd4178e118ff69ba414
Qazaroth <65128305+Qazaroth@users.noreply.github.com>
2020-05-11 09:15:29 +0800

    Updated UsernameGenerator.py

--- PythonChallenges/UsernameGenerator.py
+++ PythonChallenges/UsernameGenerator.py
@@ -6,45 +6,53 @@ wordListSrc = "https://raw.githubusercontent.com/TTT2866/Batch-username-generato
 wordListTemp = urlopen(wordListSrc)
 wordList = wordListTemp.read().__str__()
 
+noOfUsernames = 1
+
 random.seed(datetime.now())
 
+
+def genUsername():
+    usernames = ""
+
+    for i in range(1, noOfUsernames + 1):
+        firstName = newWordList[random.randint(0, newWordList.__len__())]
+        secondName = newWordList[random.randint(0, newWordList.__len__())]
+
+        randNo = random.randint(1, 999)
+
+        if randNo < 10:
+            randNo = str("00{r}".format(r=randNo))
+        elif randNo < 90:
+            randNo = str("0{r}".format(r=randNo))
+        else:
+            randNo = str(randNo)
+
+        usernames = usernames + "\n{i} - {firstName}{secondName}{randNo}".format(i=i,
+                                                                firstName=firstName.capitalize(),
+                                                                secondName=secondName,
+                                                                randNo=randNo)
+
+    return usernames
+
+
+noOfUsernames = input("How many usernames do you want to generate? ")
+
+try:
+    noOfUsernames = int(noOfUsernames)
+except ValueError:
+    print("Invalid no of usernames, defaulting to 1.")
+    noOfUsernames = 1
+
+#Checks if pyarmor is installed, since pyarmor makes it so that first 2 characters are b'
 if wordList[0] == "b" and wordList[1] == "'":
     newWordList = wordList[2:].split('\\n')
 
-    firstName = newWordList[random.randint(0, newWordList.__len__())]
-    secondName = newWordList[random.randint(0, newWordList.__len__())]
-
-    randNo = random.randint(1, 999)
-
-    if randNo < 10:
-        randNo = str("00{r}".format(r=randNo))
-    elif randNo < 90:
-        randNo = str("0{r}".format(r=randNo))
-    else:
-        randNo = str(randNo)
-
-    usernameTemp = "{firstName}{secondName}{randNo}".format(firstName=firstName.capitalize(),
-                                                            secondName=secondName,
-                                                            randNo=randNo)
+    usernameTemp = genUsername()
 
     print(usernameTemp)
 else:
     newWordList = wordList.split('\\n')
 
-    firstName = newWordList[random.randint(0, newWordList.__len__())]
-    secondName = newWordList[random.randint(0, newWordList.__len__())]
-
-    randNo = random.randint(1, 999)
-
-    if randNo < 10:
-        randNo = str("00{r}".format(r=randNo))
-    elif randNo < 90:
-        randNo = str("0{r}".format(r=randNo))
-    else:
-        randNo = str(randNo)
-
-    usernameTemp = "{firstName}{secondName}{randNo}".format(firstName=firstName.capitalize(),
-                                                            secondName=secondName,
-                                                            randNo=randNo)
+    usernameTemp = genUsername()
 
     print(usernameTemp)
